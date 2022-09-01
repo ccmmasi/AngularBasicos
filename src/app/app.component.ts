51bfb49900f677d366d9107e457689dd83d73891
Cristian Montenegro <ccmmasi@hotmail.com>
2022-09-01 13:21:22 -0500

    primer commit de versión

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+@Component({
+  selector:'app-root',
+  templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+}
---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-contador',
+    template:`
+        <h1>{{ titulo }}</h1>
+        <h3>la base es: <strong>{{ base }}</strong></h3>
+        
+        <button (click)="acumular(+base)">+{{ base }} </button>
+        
+        <span>{{ numero }}</span>
+        
+        <button (click)="acumular(-base)">-{{ base }}</button>
+    `
+})
+
+export class ContadorComponent {
+
+    titulo: string = 'Contador App';
+    numero: number = 20;
+    base  : number = 5;
+  
+    // sumar(){
+    //   this.numero += 1;
+    // }
+  
+    // restar(){
+    //   this.numero -= 1;
+    // }
+  
+    acumular(valor: number){
+      this.numero += valor;
+    }
+}
